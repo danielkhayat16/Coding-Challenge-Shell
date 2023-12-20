f03269cfd96de5d5cc7530c5342bde9c1dfbe923
Daniel Khayat <daniel.khayat8@gmail.com>
2023-12-20 21:50:42 +0200

    Adding Pipline commands, My mistake was the data structure of the passed command

--- commands.py
+++ commands.py
@@ -1,37 +1,88 @@
 import os
 from exception import *
+import subprocess
+
 
 def ls(argsNumber):
-    if(argsNumber != 0):
-        raise ArgumentError(0,argsNumber)
+    if argsNumber != 0:
+        raise ArgumentError(0, argsNumber)
     current_directory = os.getcwd()
     files = os.listdir(current_directory)
     for file in files:
         print(file)
 
+
 def pwd(argsNumber):
-    if(argsNumber != 0):
-        raise ArgumentError(0,argsNumber)
-    print (os.getcwd())
-    return(os.getcwd())
+    if argsNumber != 0:
+        raise ArgumentError(0, argsNumber)
+    print(os.getcwd())
+    return os.getcwd()
+
 
 def cat(filePath, argsNumber):
-    if(argsNumber != 1):
+    if argsNumber != 1:
         raise ArgumentError(0, argsNumber)
     try:
-        with open(filePath, 'r') as file:
+        with open(filePath, "r") as file:
             file_contents = file.read()
             print(file_contents)
     except FileNotFoundError as e:
         print(e)
 
+
 def cd(filePath, argsNumber):
     newDir = os.getcwd()
-    if(argsNumber != 1):
+    if argsNumber != 1:
         raise ArgumentError(1, argsNumber)
-    if(filePath == ".."):
+    if filePath == "..":
         strToRemove = len(newDir.split("/")[-1])
         newDir = newDir[:-strToRemove]
     else:
-        newDir += "/"+ filePath
+        newDir += "/" + filePath
     os.chdir(newDir)
+
+
+def curl(url, argument, fileName, argsNumber):
+    if argsNumber != 3:
+        raise ArgumentError(3, argsNumber)
+    result = subprocess.run(
+        "curl " + url + " " + argument + " " + fileName,
+        shell=True,
+        capture_output=True,
+        text=True,
+    )
+    print(result.stdout)
+
+
+def execute_command_pipeline(command, input_text=None):
+    try:
+        if input_text:
+            process = subprocess.Popen(
+                command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True
+            )
+            return process.communicate(input=input_text)[0]
+        else:
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, text=True)
+            return process.communicate()[0]
+    except FileNotFoundError as e:
+        print(e)
+
+
+def run_pipeline(commands):
+    commands = commands.split("|")
+    input_text = None
+
+    for cmd in commands:
+        cmd = cmd.split()
+        input_text = execute_command_pipeline(cmd, input_text)
+
+    return input_text
+
+
+def execute_single_command(command):
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        print(result.stdout.split()[0])
+
+    except FileNotFoundError as e:
+        print(e)
--- main.py
+++ main.py
@@ -1,49 +1,74 @@
 from commands import *
 from exception import CommandError
 
+
 def main():
-    print('% cssh')
-    commandListe = ["ls", "pwd", "cat", "cd"]
+    print("% cssh")
+
     userCommand = getCommandFromUSer()
-    while(userCommand[0] != "exit"):
-        try:
-            if( userCommand[0] not in commandListe):
-                raise CommandError
-            
-            if(userCommand[0] == "ls"):
-                ls(getNbArgs(userCommand=userCommand))
 
-            if(userCommand[0] == "pwd"):
-                pwd(getNbArgs(userCommand=userCommand))
-
-            if(userCommand[0] == "cat"):
-                if(len (userCommand) == 2):
-                    cat(userCommand[1], getNbArgs(userCommand=userCommand))
-                else:
-                    raise ArgumentError(1,getNbArgs(userCommand=userCommand))
-
-            if(userCommand[0] == "cd"):
-                if(len(userCommand) == 2):
-                    cd(userCommand[1], getNbArgs(userCommand=userCommand))
-                else:
-                    raise ArgumentError(1, getNbArgs(userCommand=userCommand))
-                  
-        except CommandError as e:
-            print("Error: ", e)
-            
-        except ArgumentError as e:
-            print("Error: ", e)
-        finally:
-            userCommand = getCommandFromUSer()
+    while userCommand[0] != "exit":
+        runCommand(userCommand)
+        userCommand = getCommandFromUSer()
 
     print("%")
 
+
+def runCommand(currentCommand):
+    commandListe = ["ls", "pwd", "cat", "cd", "curl"]
+    userCommand = currentCommand.split()
+    isPipelinedCommand = "|" in currentCommand
+    try:
+        if isPipelinedCommand:
+            print(run_pipeline(currentCommand))
+
+        if userCommand[0] == "ls" and not isPipelinedCommand:
+            ls(getNbArgs(userCommand=userCommand))
+
+        if userCommand[0] == "pwd" and not isPipelinedCommand:
+            pwd(getNbArgs(userCommand=userCommand))
+
+        if userCommand[0] == "cat" and not isPipelinedCommand:
+            if len(userCommand) == 2:
+                cat(userCommand[1], getNbArgs(userCommand=userCommand))
+            else:
+                raise ArgumentError(1, getNbArgs(userCommand=userCommand))
+
+        if userCommand[0] == "cd" and not isPipelinedCommand:
+            if len(userCommand) == 2:
+                cd(userCommand[1], getNbArgs(userCommand=userCommand))
+            else:
+                raise ArgumentError(1, getNbArgs(userCommand=userCommand))
+
+        if userCommand[0] == "curl" and not isPipelinedCommand:
+            if len(userCommand) == 4:
+                curl(
+                    userCommand[1],
+                    userCommand[2],
+                    userCommand[3],
+                    getNbArgs(userCommand=userCommand),
+                )
+            else:
+                raise ArgumentError(1, getNbArgs(userCommand=userCommand))
+
+        if userCommand[0] not in commandListe:
+            execute_single_command(currentCommand)
+
+    except CommandError as e:
+        print("Error: ", e)
+
+    except ArgumentError as e:
+        print("Error: ", e)
+
+
 def getCommandFromUSer():
     userCommand = input("ccsh> ")
-    return userCommand.split()
+    return userCommand
+
 
 def getNbArgs(userCommand):
-    return (len(userCommand) - 1)
+    return len(userCommand) - 1
+
 
 if __name__ == "__main__":
     main()
